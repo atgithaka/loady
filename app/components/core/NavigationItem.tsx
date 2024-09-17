92f912ad8472a562f510d8522e86bc865f3b3a14
atgithaka <atgithaka@gmail.com>
2024-09-17 09:31:29 +0300

    style: navigation item
    added a hover effect for the navigation item to change to a lighter color
    
    the previous hover effect was resulting to unnecessary changes in width that affected the layout
    
    made the text color change to gray-700

--- app/components/core/NavigationItem.tsx
+++ app/components/core/NavigationItem.tsx
@@ -2,7 +2,7 @@ export default function NavigationItem({ name, URL, active }: NameLinkProps) {
     return (
         <a
             href={URL}
-            className={active ? 'font-semibold' : 'hover:font-medium'}>
+            className={active ? 'font-semibold' : 'hover:text-gray-700'}>
             {name}
         </a>
     );
